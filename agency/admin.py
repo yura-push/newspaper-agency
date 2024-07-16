99a93c6f859037b4da035a4a3ff9f175105db389
Yura Push <youdeimos1@gmail.com>
2024-07-16 19:35:25 +0300

    customize newspaper model look in admin page

--- agency/admin.py
+++ agency/admin.py
@@ -4,6 +4,12 @@ from django.contrib.auth.admin import UserAdmin
 from agency.models import Topic, Newspaper, Redactor
 
 
+@admin.register(Newspaper)
+class NewspaperAdmin(admin.ModelAdmin):
+    list_display = ["title", "content", "published_date", "topic",]
+    list_filter = ["topic", ]
+    search_fields = ["title", ]
+
+
 admin.site.register(Topic)
-admin.site.register(Newspaper)
 admin.site.register(Redactor, UserAdmin)
