cb15cdf37428ce7e1013461dc0b8a2a6abd9f15d
Yura Push <youdeimos1@gmail.com>
2024-07-16 19:26:02 +0300

    register Topic, Newspaper and Redactor models in admin panel

--- agency/admin.py
+++ agency/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
 
-# Register your models here.
+from agency.models import Topic, Newspaper, Redactor
+
+
+admin.site.register(Topic)
+admin.site.register(Newspaper)
+admin.site.register(Redactor, UserAdmin)
