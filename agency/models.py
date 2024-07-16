725ad65d34129e1b59396826f2239b4d734a2bac
Yura Push <youdeimos1@gmail.com>
2024-07-16 17:58:17 +0300

    setup models topic, redactor, newspaper for Newspaper agency project

--- agency/models.py
+++ agency/models.py
@@ -1,3 +1,42 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Create your models here.
+from newspaper_agency import settings
+
+
+class Topic(models.Model):
+    name = models.CharField(max_length=255)
+
+    class Meta:
+        ordering = ("name",)
+
+    def __str__(self):
+        return self.name
+
+
+class Redactor(AbstractUser):
+    years_of_experience = models.IntegerField(null=True)
+
+    class Meta:
+        ordering = ("username", )
+
+
+class Newspaper(models.Model):
+    title = models.CharField(max_length=255)
+    content = models.TextField()
+    published_date = models.DateTimeField()
+    topic = models.ForeignKey(
+        Topic,
+        on_delete=models.CASCADE,
+        related_name="newspapers"
+    )
+    publishers = models.ManyToManyField(
+        settings.AUTH_USER_MODEL,
+        related_name="newspapers"
+    )
+
+    class Meta:
+        ordering = ("title",)
+
+    def __str__(self):
+        return f"{self.title} {self.content} ({self.published_date})"
