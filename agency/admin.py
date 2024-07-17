7fa0c434c7cabc94a7f893c45c3a8d3f51a730a6
Yura Push <youdeimos1@gmail.com>
2024-07-17 18:18:26 +0300

    customize the display of the redactor model in the admin panel

--- agency/admin.py
+++ agency/admin.py
@@ -11,5 +11,15 @@ class NewspaperAdmin(admin.ModelAdmin):
     search_fields = ["title", ]
 
 
+@admin.register(Redactor)
+class RedactorAdmin(UserAdmin):
+    list_display = UserAdmin.list_display + ("years_of_experience", )
+    fieldsets = UserAdmin.fieldsets + (
+        ("Additional info", {"fields": ("years_of_experience",)}),
+    )
+    add_fieldsets = UserAdmin.add_fieldsets + (
+        ("Additional info", {"fields": ("first_name", "last_name", "years_of_experience",)}),
+    )
+
+
 admin.site.register(Topic)
-admin.site.register(Redactor, UserAdmin)
