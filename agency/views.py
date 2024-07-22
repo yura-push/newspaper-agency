dad160c3432bec61a27f68887f74e68f83022cc9
Yura Push <youdeimos1@gmail.com>
2024-07-22 17:31:24 +0300

    create index view function

--- agency/views.py
+++ agency/views.py
@@ -1,3 +1,6 @@
+from django.http import HttpRequest, HttpResponse
 from django.shortcuts import render
 
-# Create your views here.
+
+def index(request: HttpRequest) -> HttpResponse:
+    return HttpResponse("<h1>Welcome to Newspaper Agency</h1>")
