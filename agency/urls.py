f56626ec4e6d34cce3e5cee3dc93ef166720fb4e
Yura Push <youdeimos1@gmail.com>
2024-08-01 16:56:55 +0300

    add topics list to url patterns

--- agency/urls.py
+++ agency/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
 
-from agency.views import index
+from agency.views import index, TopicsListView
 
 
 urlpatterns = [
-    path("", index, name="index")
+    path("", index, name="index"),
+    path("topics/", TopicsListView.as_view(), name="topics-list"),
 ]
 
 app_name = "agency"
