36f0856d129e53985cafbebfa5fac4ae6b4921f6
Yura Push <youdeimos1@gmail.com>
2024-08-09 15:50:50 +0300

    add url pattern for newspaper list view

--- agency/urls.py
+++ agency/urls.py
@@ -1,11 +1,12 @@
 from django.urls import path
 
-from agency.views import index, TopicsListView
+from agency.views import index, TopicsListView, NewspaperListView
 
 
 urlpatterns = [
     path("", index, name="index"),
-    path("topics/", TopicsListView.as_view(), name="topics-list"),
+    path("topics/", TopicsListView.as_view(), name="topic-list"),
+    path("newspapers/", NewspaperListView.as_view(), name="newspaper-list"),
 ]
 
 app_name = "agency"
