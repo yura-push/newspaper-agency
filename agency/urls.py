d225002e4133c120d730330c03a2a58b48b5a964
Yura Push <youdeimos1@gmail.com>
2024-08-09 17:32:57 +0300

    add url pattern for redactor list view

--- agency/urls.py
+++ agency/urls.py
@@ -1,12 +1,18 @@
 from django.urls import path
 
-from agency.views import index, TopicsListView, NewspaperListView
+from agency.views import (
+    index,
+    TopicsListView,
+    NewspaperListView,
+    RedactorListView
+)
 
 
 urlpatterns = [
     path("", index, name="index"),
     path("topics/", TopicsListView.as_view(), name="topic-list"),
     path("newspapers/", NewspaperListView.as_view(), name="newspaper-list"),
+    path("redactors/", RedactorListView.as_view(), name="redactor-list"),
 ]
 
 app_name = "agency"
