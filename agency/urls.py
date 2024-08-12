b7ca4f8088d877f5386cc34ecd53d9a049d56c5e
Yura Push <youdeimos1@gmail.com>
2024-08-12 20:42:54 +0300

    add url pattern for newspaper detail view

--- agency/urls.py
+++ agency/urls.py
@@ -4,7 +4,8 @@ from agency.views import (
     index,
     TopicsListView,
     NewspaperListView,
-    RedactorListView
+    RedactorListView,
+    NewspaperDetailView,
 )
 
 
@@ -13,6 +14,7 @@ urlpatterns = [
     path("topics/", TopicsListView.as_view(), name="topic-list"),
     path("newspapers/", NewspaperListView.as_view(), name="newspaper-list"),
     path("redactors/", RedactorListView.as_view(), name="redactor-list"),
+    path("newspapers/<int:pk>/", NewspaperDetailView.as_view(), name="newspaper-detail"),
 ]
 
 app_name = "agency"
