77828fece59130ed0fe04fc55d4c5bb0f1c851e1
Yura Push <youdeimos1@gmail.com>
2024-08-15 14:03:33 +0300

    add url pattern for redactor detail page

--- agency/urls.py
+++ agency/urls.py
@@ -6,6 +6,7 @@ from agency.views import (
     NewspaperListView,
     RedactorListView,
     NewspaperDetailView,
+    RedactorDetailView,
 )
 
 
@@ -15,6 +16,7 @@ urlpatterns = [
     path("newspapers/", NewspaperListView.as_view(), name="newspaper-list"),
     path("redactors/", RedactorListView.as_view(), name="redactor-list"),
     path("newspapers/<int:pk>/", NewspaperDetailView.as_view(), name="newspaper-detail"),
+    path("redactors/<int:pk>/", RedactorDetailView.as_view(), name="redactor-detail"),
 ]
 
 app_name = "agency"
