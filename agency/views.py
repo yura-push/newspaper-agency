276fba98b77a7303ebf7323f2b86bc111421e7e3
Yura Push <youdeimos1@gmail.com>
2024-08-17 19:36:01 +0300

    add counter of visits to index view function

--- agency/views.py
+++ agency/views.py
@@ -9,10 +9,13 @@ def index(request: HttpRequest) -> HttpResponse:
     num_topics = Topic.objects.count()
     num_redactors = Redactor.objects.count()
     num_newspapers = Newspaper.objects.count()
+    num_visits = request.session.get("num_visits", 0)
+    request.session["num_visits"] = num_visits + 1
     context = {
         "num_topics": num_topics,
         "num_redactors": num_redactors,
-        "num_newspapers": num_newspapers
+        "num_newspapers": num_newspapers,
+        "num_visits": num_visits + 1,
     }
     return render(request, "agency/index.html", context=context)
 
