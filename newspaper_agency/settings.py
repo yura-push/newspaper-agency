7d33d370a72284abd38a5b0ed58d18d15acf3ce8
Yura Push <youdeimos1@gmail.com>
2024-08-20 12:03:59 +0300

    add LOGIN_REDIRECT_URL to redirect from login page to homepage

--- newspaper_agency/settings.py
+++ newspaper_agency/settings.py
@@ -122,9 +122,13 @@ STATICFILES_DIRS = [
     BASE_DIR / "static",
 ]
 
+STATIC_ROOT = BASE_DIR / "staticfiles"
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
 AUTH_USER_MODEL = "agency.Redactor"
+
+LOGIN_REDIRECT_URL = "/"
