f86deb9d1e5dee1e9d07a7032e660da4271e9869
Yura Push <youdeimos1@gmail.com>
2024-09-15 19:03:46 +0300

    add url patterns for topic and newspaper class based create views

--- agency/urls.py
+++ agency/urls.py
@@ -2,8 +2,10 @@ from django.urls import path
 
 from agency.views import (
     index,
-    TopicsListView,
+    TopicListView,
+    TopicCreateView,
     NewspaperListView,
+    NewspaperCreateView,
     RedactorListView,
     NewspaperDetailView,
     RedactorDetailView,
@@ -12,8 +14,10 @@ from agency.views import (
 
 urlpatterns = [
     path("", index, name="index"),
-    path("topics/", TopicsListView.as_view(), name="topic-list"),
+    path("topics/", TopicListView.as_view(), name="topic-list"),
+    path("topics/create/", TopicCreateView.as_view(), name="topic-create"),
     path("newspapers/", NewspaperListView.as_view(), name="newspaper-list"),
+    path("newspapers/create/", NewspaperCreateView.as_view(), name="newspaper-create"),
     path("redactors/", RedactorListView.as_view(), name="redactor-list"),
     path("newspapers/<int:pk>/", NewspaperDetailView.as_view(), name="newspaper-detail"),
     path("redactors/<int:pk>/", RedactorDetailView.as_view(), name="redactor-detail"),
