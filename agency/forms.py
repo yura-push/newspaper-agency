b946d42d5f289c0111a34fe2a72868d84b17ad26
Yura Push <youdeimos1@gmail.com>
2024-09-20 15:17:12 +0300

    add newspaper creation form

--- agency/forms.py
+++ agency/forms.py
@@ -1,7 +1,8 @@
 from django import forms
+from django.contrib.auth import get_user_model
 from django.contrib.auth.forms import UserCreationForm
 
-from agency.models import Redactor
+from agency.models import Redactor, Newspaper
 
 
 class RedactorCreationForm(UserCreationForm):
@@ -14,3 +15,15 @@ class RedactorExperienceUpdateForm(forms.ModelForm):
     class Meta:
         model = Redactor
         fields = ("years_of_experience",)
+
+
+class NewspaperCreationForm(forms.ModelForm):
+    publishers = forms.ModelMultipleChoiceField(
+        queryset=get_user_model().objects.all(),
+        widget=forms.CheckboxSelectMultiple,
+        required=False,
+    )
+
+    class Meta:
+        model = Newspaper
+        fields = "__all__"
