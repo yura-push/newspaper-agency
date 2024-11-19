9415841d6f34f38a07e4c6d8acc5d50ea63ecc66
Yura Push <youdeimos1@gmail.com>
2024-11-19 15:09:18 +0200

    add SECRET_KEY

--- newspaper_agency/settings.py
+++ newspaper_agency/settings.py
@@ -10,7 +10,9 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
+from dotenv import load_dotenv
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +22,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-lh2%ltib4oqsk76u8j^3#vh@vtw7jy3^25!=cxu#4p16fi6cb&"
+load_dotenv()
+SECRET_KEY = os.getenv("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
