1ba79ec4aabe238d7ef4091b721ffe43946e253e
Yura Push <youdeimos1@gmail.com>
2024-12-11 16:40:59 +0200

    depoloyment preparations

--- newspaper_agency/settings/prod.py
+++ newspaper_agency/settings/prod.py
@@ -5,6 +5,10 @@ DEBUG = False
 
 ALLOWED_HOSTS = ["127.0.0.1", "localhost"]
 
+RENDER_EXTERNAL_HOSTNAME = os.environ.get("RENDER_EXTERNAL_HOSTNAME")
+if RENDER_EXTERNAL_HOSTNAME:
+   ALLOWED_HOSTS.append(RENDER_EXTERNAL_HOSTNAME)
+
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 DATABASES = {
