d197d7c468223559d3b6705d7b936b4ed0cad1ac
Yura Push <youdeimos1@gmail.com>
2024-12-17 14:54:46 +0200

    add allowed host

--- newspaper_agency/settings/prod.py
+++ newspaper_agency/settings/prod.py
@@ -3,7 +3,7 @@ from .base import *
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ["127.0.0.1", "localhost"]
+ALLOWED_HOSTS = ["127.0.0.1", "localhost", "https://newspaper-agency-z8ky.onrender.com"]
 
 RENDER_EXTERNAL_HOSTNAME = os.environ.get("RENDER_EXTERNAL_HOSTNAME")
 if RENDER_EXTERNAL_HOSTNAME:
